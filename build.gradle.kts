11cee45904fc8713566cd0445c2eeb961d970f64
yaquelin2305 <ya.rugel@duocuc.cl>
2025-12-06 18:00:23 -0300

    migrando a la nube de la base de datos a Railway/MySQL,

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,31 +23,17 @@ repositories {
 
 dependencies {
 
-
     implementation("org.springframework.boot:spring-boot-starter-web")
-
-
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-
-
     implementation("org.springframework.boot:spring-boot-starter-validation")
-
-
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-
-
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 
 
-    runtimeOnly("org.postgresql:postgresql:42.7.3")
-
-
+    runtimeOnly("com.mysql:mysql-connector-j:8.3.0")
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
-
-
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
