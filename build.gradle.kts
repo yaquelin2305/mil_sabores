06d2d3c06fb06776684881f0c2ddf3858da184fa
yaquelin2305 <ya.rugel@duocuc.cl>
2025-12-08 18:25:37 -0300

    Downgrade Kotlin to 1.9.25 (Spring Boot compatible)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
-    kotlin("jvm") version "2.2.21"
-    kotlin("plugin.spring") version "2.2.21"
-    kotlin("plugin.jpa") version "2.2.21"
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    kotlin("plugin.jpa") version "1.9.25"
 
     id("org.springframework.boot") version "3.3.5"
     id("io.spring.dependency-management") version "1.1.7"
 }
+
 
 group = "com.pasteleria"
 version = "0.0.1-SNAPSHOT"
